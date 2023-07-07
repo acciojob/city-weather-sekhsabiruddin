826c9408f2aed32e516f232df64aea1f3df5fe64
sabir <sksabiruddin81@gmail.com>
2023-07-07 22:38:53 +0530

    submitting code-Fri Jul 07 2023 22:38:49 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import Fetch from "./Fetch";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Fetch />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
